a051b6c82e9c6d172b6b7b09f695b7f6ff75d6c7
Sandro M. dos Santos <sandromarista@gmail.com>
2025-10-03 18:46:26 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/pessoal/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.pessoal.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pessoal.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
